74e5068df2e4eda4735b8aaf9964ce770cc56b8a
unknown <hn012041211998@gmail.com>
2022-03-21 10:47:02 +0700

    done

--- src/qlhs.sql
+++ src/qlhs.sql
@@ -1,7 +1,7 @@
-create database quanlyhocvien;
-use quanlyhocvien;
+create database quanlyhocvien1;
+use quanlyhocvien1;
 create table class(
-    id int not null primary key ,
+    id int not null primary key auto_increment,
     name varchar(255) not null ,
     language varchar(255) ,
     description varchar(255)
@@ -24,8 +24,8 @@ create table course(
     name varchar(255) not null ,
     description varchar(255)
 );
-create table point1(
-    id int ,
+create table point(
+    id int primary key not null ,
     course_id int,
     student_id int,
     point float,
@@ -33,11 +33,11 @@ create table point1(
     foreign key (course_id) references course(id),
     foreign key (student_id) references student(student_id)
 );
-insert into class(id, name, language, description) VALUES (01,'1A','Viet Nam','khong co');
-insert into class(id, name, language, description) VALUES (02,'1B','Viet Nam','khong co');
-insert into class(id, name, language, description) VALUES (03,'1C','Viet Nam','khong co');
-insert into class(id, name, language, description) VALUES (04,'1D','Viet Nam','khong co');
-insert into class(id, name, language, description) VALUES (05,'1E','Viet Nam','khong co');
+insert into class(id, name, language, description) VALUES (1,'1A','Viet Nam','khong co');
+insert into class( name, language, description) VALUES ('1B','Viet Nam','khong co');
+insert into class( name, language, description) VALUES ('1C','Viet Nam','khong co');
+insert into class(name, language, description) VALUES ('1D','Viet Nam','khong co');
+insert into class( name, language, description) VALUES ('1E','Viet Nam','khong co');
 select * from class;
 insert into address(id, address) VALUES (100,'HN');
 insert into address(id, address) VALUES (101,'HN');
@@ -46,15 +46,15 @@ insert into address(id, address) VALUES (103,'HN');
 insert into address(id, address) VALUES (104,'DN');
 select *from address;
 insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (10,'Tran Van A',100,26,'123',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (11,'Nguyen Van A',100,18,'124',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (12,'Nguyen Van A',100,18,'125',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (13,'Nguyen Van A',100,23,'126',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (14,'Vu Van B',100,43,'127',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (15,'Nguyen Van A',100,18,'128',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (16,'Nguyen Van A',100,55,'129',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (17,'Nguyen Van B',100,44,'130',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (18,'Nguyen Van A',100,18,'131',1);
-insert into student(student_id, fullname, address_id, age, phone, class_id) VALUES (19,'Vu Van A',100,14,'132',1);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',103,18,'124',4);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',101,18,'125',1);
+insert into student(fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',100,23,'126',2);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Vu Van B',100,43,'127',3);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',102,16,'128',1);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',103,55,'129',2);
+insert into student(fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van B',100,44,'130',1);
+insert into student( fullname, address_id, age, phone, class_id) VALUES ('Nguyen Van A',102,15,'131',4);
+insert into student(fullname, address_id, age, phone, class_id) VALUES ('Vu Van A',104,16,'132',5);
 select * from student;
 insert into course(id, name, description) VALUES (1001,'Toan','khong co');
 insert into course(id, name, description) VALUES (1002,'ly','khong co');
@@ -62,7 +62,20 @@ insert into course(id, name, description) VALUES (1003,'Hoa','khong co');
 insert into course(id, name, description) VALUES (1004,'Sinh','khong co');
 insert into course(id, name, description) VALUES (1005,'Su','khong co');
 select * from course;
-select * from student where fullname like '%Nguyen%';
-select * from student where fullname like '%B%';
+insert into point(id, course_id, student_id, point) VALUES (1,1001,10,85);
+insert into point(id, course_id, student_id, point) VALUES (2,1001,11,65);
+insert into point(id, course_id, student_id, point) VALUES (3,1002,12,75);
+insert into point(id, course_id, student_id, point) VALUES (4,1002,13,80);
+insert into point(id, course_id, student_id, point) VALUES (5,1002,14,25);
+insert into point(id, course_id, student_id, point) VALUES (6,1003,15,80);
+insert into point(id, course_id, student_id, point) VALUES (7,1003,16,55);
+insert into point(id, course_id, student_id, point) VALUES (8,1004,17,95);
+insert into point(id, course_id, student_id, point) VALUES (9,1005,18,90);
+select * from point;
+select * from student where fullname like 'Nguyen%';
+select * from student where fullname like '%B';
 select * from student where age between 15 and 18;
 select * from student where student_id = 12 or student_id = 14;
+select name, count(name) from class join student s on class.id = s.class_id group by name;
+select address, count(address) from address join student s on address.id = s.address_id group by address;
+select name, avg(point) from course join point p on course.id = p.course_id group by name;
